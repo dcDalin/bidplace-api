a74d9419b3bedccf493f213ff63516fdd311636f
Dalin  Oluoch <dc_dalin@Dalins-MacBook-Pro.local>
2020-07-03 01:33:51 +0300

    Setup babel

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send("test");
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
